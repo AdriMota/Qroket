639c9943b18856f26a8f7e50d7f05f31c442b4fd
Heloise Cuche <heloise.cuche@heig-vd.ch>
2023-01-26 10:38:16 +0100

    add user's animals

--- src/app/annonces/annonces.component.ts
+++ src/app/annonces/annonces.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth/auth.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-annonces-component',
@@ -10,23 +12,41 @@ import { environment } from 'src/environments/environment';
 export class AnnoncesComponent implements OnInit {
 
   animals : any;
+  user: string;
+  animal: any;
+  animalFilter: any;
+
   @Input() showLost: boolean;
   @Input() showFind: boolean;
+  @Input() showAll: boolean;
 
-  constructor(private http: HttpClient) { 
+
+  constructor(private http: HttpClient, private authService: AuthService,) { 
     this.getAnimals();
+  }
+ 
+  ngOnInit() {
+    this.authService.getUser$().pipe(filter(user => user != null),).subscribe(user => {
+      this.user = user.id
+    });
   }
 
   getAnimals() {
     this.http.get( environment.apiUrl + 'animals').subscribe(
       (response) => {
-        this.animals = response;
-        this.animals.sort((a, b) => a.date.localeCompare(b.date));
+        if(!this.showAll){
+          this.animalFilter = response
+          this.animals = this.animalFilter.filter(a => a.user == this.user)
+        }
+        if(this.showAll){
+          this.animals = response;
+          this.animals.sort((a, b) => a.date.localeCompare(b.date));
+        }
       },
       (error) => {
         console.log(error);
       }
     );
   }
-    ngOnInit() { }
+
   }
