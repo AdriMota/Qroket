ebcc7b4a785f795bb0f15b273638eedd93f5f7c7
Heloise Cuche <heloise.cuche@heig-vd.ch>
2023-01-27 21:22:58 +0100

    add annonces animal dynamic modal

--- src/app/modals/add-animal/add-animal.service.ts
+++ src/app/modals/add-animal/add-animal.service.ts
@@ -6,7 +6,6 @@ import { AddAnimalComponent } from "./add-animal.component";
 })
 
 export class ModalService {
-    //peut contenir des vues hôtes.
     private rootViewContainer: ViewContainerRef;
 
     constructor(private viewContainerRef: ViewContainerRef) {   
@@ -15,27 +14,22 @@ export class ModalService {
     setRootViewContainerRef(viewContainerRef: any) {
         this.rootViewContainer = viewContainerRef;
     }
-
-    //ajouter un composant. Reçoit les paramètres à afficher
-    async addDynamicComponent(modalTitle: string, modalText: string, modalLocation: string, modalAnimalId: number) {
-
-        console.log("go 2")
-
-        const component = this.viewContainerRef.createComponent(AddAnimalComponent);
+    
+    //lorsque le premier component s'ajoute dans un viewhote, il ne se passe rien. Pourquoi ? 
+    async addDynamicComponent(modalTitle: string, modalText: string, modalLocation: string, modalAnimalId: number, animalUser: number) {
+        const component = this.rootViewContainer.createComponent(AddAnimalComponent);
 
         component.instance.name = modalTitle;
         component.instance.description = modalText;
         component.instance.location = modalLocation;
         component.instance.id = modalAnimalId;
+        component.instance.animalUser = animalUser;
 
-        //écouter le click, pour pouvoir supprimer le composant
         component.instance.modalClose.subscribe(() => this.removeDynamicComponent(component))
 
         if (component.hostView && !component.hostView.destroyed) {
             this.rootViewContainer.insert(component.hostView);
-        //     console.log("dans la boucle cheloue")
         }
-
     }
 
     onWillDismiss(event: any) {
