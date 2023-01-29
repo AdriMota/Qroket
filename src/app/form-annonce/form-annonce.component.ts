08b151ca82a2015495416170e2b8ff7a70889425
Adriana Mota <adrianamota17@hotmail.com>
2023-01-29 13:58:20 +0100

    Form annonce

--- src/app/form-annonce/form-annonce.component.ts
+++ src/app/form-annonce/form-annonce.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-form-annonce',
@@ -8,9 +12,59 @@ import { ModalController } from '@ionic/angular';
 })
 export class FormAnnonceComponent implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  showForm: boolean;
+  showAnswerApi: boolean = false;
 
-  ngOnInit() {}
+  name: string = '';
+  age: string = '';
+  //age: number = '';
+  description: string = '';
+  fur: string = '';
+  date: string = '';
+  type: string = '';
+  location: string = '';
+  user: string = '';
+
+  animal: object = {};
+
+  constructor(
+    private modalController: ModalController,
+    private authService: AuthService,
+    private http: HttpClient
+    ) { }
+
+  ngOnInit() {
+    this.showForm = true;
+    this.showAnswerApi = false;
+
+    this.authService.getUser$().subscribe(user => {
+     this.user = user.id;
+    });
+  }
+
+  submitAnnonce() {
+    // this.sharedData.showAnswer();
+
+    this.showForm = false;
+    this.showAnswerApi = true;
+
+    const data = {
+      name: this.name,
+      age: this.age,
+      description: this.description,
+      fur: this.fur,
+      date: this.date,
+      type: this.type,
+      location: this.location,
+      user: this.user
+    }
+
+    console.log(data);
+    console.log(`${environment.apiUrl}animals`);
+
+    //this.http.post(`${environment.apiUrl}animals`, data);
+
+  }
 
   cancel() {
     return this.modalController.dismiss(null, 'cancel');
