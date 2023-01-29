31b8e729d10648f6297ce7a825c0111a0619679b
Adriana Mota <adrianamota17@hotmail.com>
2023-01-29 16:49:55 +0100

    modal update

--- src/app/animal-map/animal-map.component.ts
+++ src/app/animal-map/animal-map.component.ts
@@ -3,6 +3,8 @@ import { ModalController, NavParams } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from "@angular/common/http";
 import { PictureService } from 'src/app/picture/picture.service';
+import { AuthService } from '../auth/auth.service';
+import { filter } from 'rxjs';
 //import { Buffer } from 'buffer';
 
 
@@ -30,47 +32,64 @@ export class AnimalMapComponent implements OnInit {
   phoneUser: number;
   pictureUser: string;
 
+  idUserAuth: string;
+
+  yourAnnouncement: boolean = false;
+
+  newData: any;
+  property: string;
+
   constructor(
     private modalCtrl: ModalController,
     private navParams: NavParams,
     public http: HttpClient,
-    private pictureService: PictureService
+    private pictureService: PictureService,
+    private auth: AuthService
   ) { 
     this.idAnimal = this.navParams.get('idAnimal');
-
-    
-    // Make an HTTP request to retrieve the animal.
-    this.http.get(`${environment.apiUrl}animals/${this.idAnimal}`).subscribe((animal) => {
-        this.nameAnimal = animal["name"];
-        this.ageAnimal = animal["age"];
-        this.descriptionAnimal = animal["description"];
-        this.furAnimal = animal["fur"];
-        this.userAnimal = animal["user"];
-        this.locationAnimal = animal["location"];
-        this.picturesAnimal = animal["pictures"];
-
-        // Make an HTTP request to retrieve the user.
-        this.http.get(`${environment.apiUrl}users/${this.userAnimal}`).subscribe((user) => {
-          this.firstnameUser = user["firstname"];
-          this.lastnameUser = user["lastname"];
-          this.emailUser = user["email"];
-          this.phoneUser = user["fuphoner"];
-          this.pictureUser = user["picture"];
-
-          //this.takePicture();
-        });
-    });
   }
 
   ngOnInit() {
-    
+    // Récupérer les informations de l'user connecté
+    this.auth.getUser$().pipe(filter(user => user != null),).subscribe(user => {
+      this.idUserAuth = user.id;
+    });
+
+    // Make an HTTP request to retrieve the animal.
+    this.http.get(`${environment.apiUrl}animals/${this.idAnimal}`).subscribe((animal) => {
+      this.nameAnimal = animal["name"];
+      this.ageAnimal = animal["age"];
+      this.descriptionAnimal = animal["description"];
+      this.furAnimal = animal["fur"];
+      this.userAnimal = animal["user"];
+      this.locationAnimal = animal["location"];
+      this.picturesAnimal = animal["pictures"];
+
+      // Make an HTTP request to retrieve the user.
+      this.http.get(`${environment.apiUrl}users/${this.userAnimal}`).subscribe((user) => {
+        this.firstnameUser = user["firstname"];
+        this.lastnameUser = user["lastname"];
+        this.emailUser = user["email"];
+        this.phoneUser = user["fuphoner"];
+        this.pictureUser = user["picture"];
+
+        if(this.userAnimal == this.idUserAuth) {
+          this.yourAnnouncement = true;
+          console.log("Tu es le proprio de cette beauté", this.yourAnnouncement)
+        }
+
+        //this.takePicture();
+      });
+    });
   }
 
   cancel() {
+    this.yourAnnouncement = false;
     return this.modalCtrl.dismiss(null, 'cancel');
   }
 
   confirm() {
+    this.yourAnnouncement = false;
     return this.modalCtrl.dismiss(null, 'confirm');
   }
 
@@ -80,4 +99,7 @@ export class AnimalMapComponent implements OnInit {
     });
   }
 
+  deleteAnnouncement() {
+    console.log("Annonce supprimée")
+  }
 }
